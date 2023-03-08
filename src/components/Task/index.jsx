4b06f7959c0b785ddbbba3e4fe76ef4405c78808
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-03-08 03:27:21 +0000

    created task component

---
+++ src/components/Task/index.jsx
@@ -0,0 +1,40 @@
+// Scoped Styles
+import './Task.css'
+
+//External plugins
+import { MdDoneAll, MdCancel } from 'react-icons/md'
+
+const Task = ({ task, theme, completeTask }) => {
+  const handleComplete = (e) => {
+    e.preventDefault()
+    completeTask(task.id)
+  }
+
+  return (
+    <div className={`box-task-container box-task-container-${theme()}`}>
+      <div className='box-task'>
+        <div
+          className={
+            `box-task-title box-task-title-${theme()} 
+            ${task.completed ? 'line-through' : ''}`
+          }
+        >
+          {task.title}
+        </div>
+        <div className={`${task.completed ? 'line-through' : ''}`}>{task.description}</div>
+      </div>
+      <div className='box-task-action'>
+        <button
+          className={`box-task-btn box-task-btn-completed-${theme()}`}
+          onClick={handleComplete}
+        >
+          <MdDoneAll size={24} />
+        </button>
+        <button className={`box-task-btn box-task-btn-remove-${theme()}`}>
+          <MdCancel size={24} />
+        </button>
+      </div>
+    </div>
+  )
+}
+export default Task
