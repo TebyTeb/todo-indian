222d984d6378aa3a614467745973217ad702d11b
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-03-08 00:19:11 +0000

    App title, theme switcher & color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
+// Global Styles
 import './App.css'
+
+// Hooks
+import { useState } from 'react'
+
+//External plugins
+import Switch from 'react-switch'
+import { BsSunFill, BsFillMoonStarsFill } from 'react-icons/bs'
 
 function App() {
 
-  return (
-    <div>
+  const [dark, setDark] = useState(true)
 
+  const theme = () => {
+    return dark ? 'dark' : 'light'
+  }
+
+  return (
+    <div className={`app app-${theme()}`}>
+      <div className={`app-title-container app-title-container${theme()}`}>
+        <h1 className='app-title'>ToDo App</h1>
+      </div>
+
+      <Switch
+        checked={dark}
+        onChange={() => setDark(!dark)}
+        uncheckedIcon={<BsSunFill className='theme-switch' />}
+        checkedIcon={<BsFillMoonStarsFill className='theme-switch' />}
+      />
+
+      {/* <TaskContainer tasks={tasks} setTasks={setTasks} dark={dark} /> */}
     </div>
   )
 }
